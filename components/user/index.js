66127967c547c54604762a43b686eb9266a75964
Qquanwei <quanwei9958@126.com>
2022-09-15 00:05:03 +0800

    feat: user

---
+++ components/user/index.js
@@ -0,0 +1,63 @@
+import { useEffect, useState, useCallback } from 'react';
+import classNames from 'classnames';
+import Modal from 'components/modal';
+import Script from 'next/script'
+
+
+function User() {
+    const [isLogin, setLogin] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(false);
+
+    useEffect(() => {
+        window.onGoogleSignIn = function (googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.          
+        }
+
+        return () => {
+            window.onGoogleSignIn = null;
+        }
+    }, []);
+
+    const onClick = useCallback(() => {
+        setShowLoginModal(true);
+    }, []);
+
+    const onModalClose = useCallback(() => {
+        setShowLoginModal(false);
+    }, []);
+
+    return (
+        <div>
+            <div
+                onClick={onClick}
+                className={
+                    classNames("flex justify-center items-center text-xs select-none cursor-pointer outline outline-gray-500/50 outline-1 outline-offset-2 w-[50px] h-[50px] rounded-full", {
+                        'bg-gray-300': !isLogin
+                    })
+                }>
+                {
+                    isLogin ? '' : '登录'
+                }
+
+            </div>
+            <Modal show={showLoginModal} onClose={onModalClose}>
+                <h1 className="text-gray-500">
+                    第三方登录授权
+                </h1>
+                <div className="flex items-center justify-center flex-col mx-auto mt-[20px] w-[300px]">
+                    <div className="g-signin2 h-[36px]" data-onsuccess="onGoogleSignIn"></div>
+                    <a className="github-login block w-full mt-[10px] h-[36px]" target="_blank" noreferer="true" href="https://github.com/login/oauth/authorize?login&client_id=c7b4ad0b9f7f6b38da81" >
+                        Github
+                    </a>
+                </div>
+            </Modal>
+            <Script src="https://apis.google.com/js/platform.js" async defer></Script>
+        </div>
+    );
+}
+
+export default User;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link';
+import User from 'components/user';
 
 export default function Home() {
   return (
@@ -8,9 +9,13 @@ export default function Home() {
         <title>Gante! 高效的项目管理，流程图在线工具</title>
         <meta name="description" content="Gante是一款稳定高效的项目管理工具, 可靠，稳定，高效，免费！" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-signin-client_id" content="499253238732-j69pk534uht0ce1h8vg1ts8epak5rgm8.apps.googleusercontent.com" />
       </Head>
 
       <main className="fixed left-0 right-0 bottom-0 top-0">
+        <header className="z-10 absolute right-0 pr-2 pt-2">
+          <User />
+        </header>
         <div className="w-full h-full bg-[url('/gante-first.png')] bg-cover " >
         </div>
         <div className="text-black h-[300px] flex items-center text-blod text-[30px] absolute top-0 shadow-lg shadow-blue/50 shadow px-10 bg-[#003f5c] w-full">
@@ -22,6 +27,7 @@ export default function Home() {
           </div>
         </div>
       </main>
+
     </div>
   )
 }
