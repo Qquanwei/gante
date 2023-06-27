e804ce617c987c3b01c8cd935a055dfdba95d2ef
李权威 <liquanwei@corp.netease.com>
2023-06-27 15:43:22 +0800

    feat: psql

--- config/config.online.js
+++ config/config.online.js
@@ -1,7 +1,16 @@
 const mongo_uname = process.env.GANTE_MONGO_UNAME;
 const mongo_pass = process.env.GANTE_MONGO_PASS;
+const psql_uname = process.env.GANTE_PSQL_UNAME;
+const psql_pass = process.env.GANTE_PSQL_PASS;
 
 module.exports = {
   MONGO_ADDR: `mongodb://${mongo_uname}:${mongo_pass}@dds-bp10dc1f0e38fad41.mongodb.rds.aliyuncs.com:3717/gante_store?authSource=admin`,
+  pg: {
+    user: psql_uname,
+    password: psql_pass,
+    host: 'pgm-bp14z805s1wrc544.rwlb.rds.aliyuncs.com',
+    database: 'gantedb',
+    port: 5432
+  },
   cookieDomain: '.gante.link'
 };
